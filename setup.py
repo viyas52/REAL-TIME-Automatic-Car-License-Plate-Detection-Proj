c46625774b6d9da48def3f94e4e5222e6564afb8
viyas52 <vyas.illusionist@gmail.com>
2024-08-23 19:34:47 +0530

    requirements and setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ANPR',
+    version= '0.0.0',
+    author= 'viyas52',
+    author_email= 'vyas.illusionist@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
