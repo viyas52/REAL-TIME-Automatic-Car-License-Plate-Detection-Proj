fe2beef802198b1f1f2426ef63ac72420f1bf544
viyas52 <vyas.illusionist@gmail.com>
2024-09-27 22:44:34 +0530

    final commit

--- ALPD/components/data_ingestion.py
+++ ALPD/components/data_ingestion.py
@@ -2,6 +2,4 @@ import cv2
 
 def read_video(input_video_path):
     cap = cv2.VideoCapture(input_video_path)
-    if not cap.isOpened():
-        raise Exception(f"Error: Could not open video '{input_video_path}'")
     return cap
